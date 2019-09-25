fa860c6a5240a19aa7fef34cc5e3066e16ac92f2
mator2006 <mator2006@gmail.com>
2019-09-25 15:29:28 +0800

    youhua2

--- http-get.go
+++ http-get.go
@@ -15,42 +15,62 @@ import (
 )
 
 type hl7 struct {
-	HL7order struct {
+	LinkOption struct {
 		Urlhost     string
 		Suburl1     string
 		Suburl2     string
 		Requestmode string
 		Cookieid    string
 		ContentType string
-		Pagesize    string
+		Jsonkey0    string
 		Jsonkey1    string
 		Jsonkey2    string
 		Jsonkey3    string
 		Codetype    string
-		chuancan    string
+	}
+	RquestOption struct {
+		PageNum             string
+		PageSize            string
+		GiveUpCount         string
+		LastTouchTimeRange1 string
+		LastTouchTimeRange2 string
+		LastTouchTimeRange3 string
+		LastTouchTimeRange4 string
+		StartPageNum        string
+		EndPageNum          string
+		LastTouchStartDate  string
+		LastTouchEndDate    string
+		chuancan            string
 	}
 }
 
 func main() {
-
-	Qingqiu1t := `{"pageNum":1,"pageSize":`
-	Qingqiu1b := `,"condition":{"giveUpCount":5,"lastTouchTimeRange":["2019-08-17T08:25:15.342Z","2019-09-15T08:25:15.342Z"],"startPageNum":100,"endPageNum":100,"lastTouchStartDate":"2019-08-17 00:00:00","lastTouchEndDate":"2019-09-15 23:59:59"}}`
-	Qingqiu2t := `{"customerId":"`
-	Qingqiu2b := `"}`
 
 	t := time.Now()
 	Outputfilename := t.Format("20060102150405") + ".CSV"
 	Inifilename := "config.ini"
 
 	hl7data := readinifile(Inifilename)
-	hl7data.HL7order.chuancan = Qingqiu1t + hl7data.HL7order.Pagesize + Qingqiu1b
+
+	hl7data.RquestOption.chuancan = `{` +
+		`"pageNum": ` + hl7data.RquestOption.PageNum + `,` +
+		`"pageSize": ` + hl7data.RquestOption.PageSize + `,` +
+		`"condition": {` +
+		`"giveUpCount": ` + hl7data.RquestOption.GiveUpCount + `,` +
+		`"lastTouchTimeRange": ["` + hl7data.RquestOption.LastTouchTimeRange1 + `T` + hl7data.RquestOption.LastTouchTimeRange2 + `.342Z", "` + hl7data.RquestOption.LastTouchTimeRange3 + `T` + hl7data.RquestOption.LastTouchTimeRange4 + `.342Z"],` +
+		`"startPageNum": ` + hl7data.RquestOption.StartPageNum + `,` +
+		`"endPageNum": ` + hl7data.RquestOption.EndPageNum + `,` +
+		`"lastTouchStartDate": "` + hl7data.RquestOption.LastTouchStartDate + `00:00:00",` +
+		`"lastTouchEndDate": "` + hl7data.RquestOption.LastTouchEndDate + `23:59:59"` +
+		`}` +
+		`}`
 	customerId := strings.Split(Getdata(hl7data, getpage(hl7data)), ",")
 
-	hl7data.HL7order.Jsonkey1 = hl7data.HL7order.Jsonkey3
+	hl7data.LinkOption.Jsonkey1 = hl7data.LinkOption.Jsonkey3
 	customerName := strings.Split(Getdata(hl7data, getpage(hl7data)), ",")
 
-	hl7data.HL7order.Suburl1 = hl7data.HL7order.Suburl2
-	hl7data.HL7order.Jsonkey1 = hl7data.HL7order.Jsonkey2
+	hl7data.LinkOption.Suburl1 = hl7data.LinkOption.Suburl2
+	hl7data.LinkOption.Jsonkey1 = hl7data.LinkOption.Jsonkey2
 
 	f, err := os.Create(Outputfilename)
 	if err != nil {
@@ -60,14 +80,14 @@ func main() {
 	f.WriteString("\xEF\xBB\xBF")
 	w := csv.NewWriter(f)
 
-	enc := mahonia.NewEncoder(hl7data.HL7order.Codetype) //GO默认编码方式为UTF-8，但是Windows识别编码默认为ANSI（简中为GBK），故Windows下使用需要转码，enc.ConvertString为具体转码实现
+	enc := mahonia.NewEncoder(hl7data.LinkOption.Codetype) //GO默认编码方式为UTF-8，但是Windows识别编码默认为ANSI（简中为GBK），故Windows下使用需要转码，enc.ConvertString为具体转码实现
 
 	w.Write([]string{enc.ConvertString("序号"), enc.ConvertString("CID"), enc.ConvertString("学员姓名"), enc.ConvertString("学员性别"), enc.ConvertString("学员手机号"), enc.ConvertString("qq"), enc.ConvertString("微信"), enc.ConvertString("项目(必填)"), enc.ConvertString("学历"), enc.ConvertString("年龄"), enc.ConvertString("证件类型"), enc.ConvertString("证件号码"), enc.ConvertString("客户来源"), enc.ConvertString("创建人"), enc.ConvertString("创建时间(yyyy-MM-dd HH:mm:ss)"), enc.ConvertString("地域(必填)"), enc.ConvertString("归属人"), enc.ConvertString("回访次数"), enc.ConvertString("下次回访时间(yyyy-MM-dd HH:mm:ss)"), enc.ConvertString("备注")})
 
 	for count1, cid1 := range customerId { //此处可以声明两个变量，第一个是数组位置的值，第二个代表该数组的值
 		cid1 = strings.Trim(strings.Trim(cid1, `[`), `]`)
-		hl7data.HL7order.chuancan = Qingqiu2t + cid1 + Qingqiu2b
-		fmt.Println("Processed " + strconv.Itoa(count1+1) + "/" + hl7data.HL7order.Pagesize + " item data.customerId is " + cid1)
+		hl7data.RquestOption.chuancan = `{"` + hl7data.LinkOption.Jsonkey0 + `":"` + cid1 + `"}`
+		fmt.Println("Processed " + strconv.Itoa(count1+1) + "/" + hl7data.RquestOption.PageSize + " item data.customerId is " + cid1)
 
 		w.Write([]string{enc.ConvertString(strconv.Itoa(count1 + 1)), enc.ConvertString(cid1), enc.ConvertString(strings.Trim(strings.Trim(strings.Trim(customerName[count1], `[`), `]`), `"`)), "", Getdata(hl7data, getpage(hl7data))})
 		w.Flush()
@@ -77,12 +97,12 @@ func main() {
 func getpage(hl7data hl7) string {
 
 	client1 := &http.Client{}
-	req, err := http.NewRequest(hl7data.HL7order.Requestmode, hl7data.HL7order.Urlhost+hl7data.HL7order.Suburl1, strings.NewReader(hl7data.HL7order.chuancan)) //发送请求
+	req, err := http.NewRequest(hl7data.LinkOption.Requestmode, hl7data.LinkOption.Urlhost+hl7data.LinkOption.Suburl1, strings.NewReader(hl7data.RquestOption.chuancan)) //发送请求
 	if err != nil {
 		fmt.Println(err)
 	}
-	req.Header.Set("Content-Type", hl7data.HL7order.ContentType) //请求配置连接类型
-	req.Header.Set("Cookie", hl7data.HL7order.Cookieid)          //请求配置cookie
+	req.Header.Set("Content-Type", hl7data.LinkOption.ContentType) //请求配置连接类型
+	req.Header.Set("Cookie", hl7data.LinkOption.Cookieid)          //请求配置cookie
 
 	resp, err := client1.Do(req) //接收返回信息
 	if err != nil {
@@ -98,7 +118,7 @@ func getpage(hl7data hl7) string {
 }
 
 func Getdata(hl7data hl7, jsonstr string) string {
-	resulta := gjson.Get(jsonstr, hl7data.HL7order.Jsonkey1)
+	resulta := gjson.Get(jsonstr, hl7data.LinkOption.Jsonkey1)
 	return resulta.String()
 }
 
@@ -114,17 +134,29 @@ func readinifile(Inifilename string) hl7 {
 		if err != nil {
 			fmt.Println(err, "Failed to parseconfigure file!")
 		} else {
-			hl7data.HL7order.Urlhost = strings.TrimSpace(config.HL7order.Urlhost)
-			hl7data.HL7order.Suburl1 = strings.TrimSpace(config.HL7order.Suburl1)
-			hl7data.HL7order.Suburl2 = strings.TrimSpace(config.HL7order.Suburl2)
-			hl7data.HL7order.Requestmode = strings.TrimSpace(config.HL7order.Requestmode)
-			hl7data.HL7order.Cookieid = "JSESSIONID=" + strings.TrimSpace(config.HL7order.Cookieid)
-			hl7data.HL7order.ContentType = strings.TrimSpace(config.HL7order.ContentType)
-			hl7data.HL7order.Pagesize = strings.TrimSpace(config.HL7order.Pagesize)
-			hl7data.HL7order.Jsonkey1 = strings.TrimSpace(config.HL7order.Jsonkey1)
-			hl7data.HL7order.Jsonkey2 = strings.TrimSpace(config.HL7order.Jsonkey2)
-			hl7data.HL7order.Jsonkey3 = strings.TrimSpace(config.HL7order.Jsonkey3)
-			hl7data.HL7order.Codetype = strings.TrimSpace(config.HL7order.Codetype)
+			hl7data.LinkOption.Urlhost = strings.TrimSpace(config.LinkOption.Urlhost)
+			hl7data.LinkOption.Suburl1 = strings.TrimSpace(config.LinkOption.Suburl1)
+			hl7data.LinkOption.Suburl2 = strings.TrimSpace(config.LinkOption.Suburl2)
+			hl7data.LinkOption.Requestmode = strings.TrimSpace(config.LinkOption.Requestmode)
+			hl7data.LinkOption.Cookieid = "JSESSIONID=" + strings.TrimSpace(config.LinkOption.Cookieid)
+			hl7data.LinkOption.ContentType = strings.TrimSpace(config.LinkOption.ContentType)
+			hl7data.LinkOption.Jsonkey0 = strings.TrimSpace(config.LinkOption.Jsonkey0)
+			hl7data.LinkOption.Jsonkey1 = strings.TrimSpace(config.LinkOption.Jsonkey1)
+			hl7data.LinkOption.Jsonkey2 = strings.TrimSpace(config.LinkOption.Jsonkey2)
+			hl7data.LinkOption.Jsonkey3 = strings.TrimSpace(config.LinkOption.Jsonkey3)
+			hl7data.LinkOption.Codetype = strings.TrimSpace(config.LinkOption.Codetype)
+
+			hl7data.RquestOption.PageNum = strings.TrimSpace(config.RquestOption.PageNum)
+			hl7data.RquestOption.PageSize = strings.TrimSpace(config.RquestOption.PageSize)
+			hl7data.RquestOption.GiveUpCount = strings.TrimSpace(config.RquestOption.GiveUpCount)
+			hl7data.RquestOption.LastTouchTimeRange1 = strings.TrimSpace(config.RquestOption.LastTouchTimeRange1)
+			hl7data.RquestOption.LastTouchTimeRange2 = strings.TrimSpace(config.RquestOption.LastTouchTimeRange2)
+			hl7data.RquestOption.LastTouchTimeRange3 = strings.TrimSpace(config.RquestOption.LastTouchTimeRange3)
+			hl7data.RquestOption.LastTouchTimeRange4 = strings.TrimSpace(config.RquestOption.LastTouchTimeRange4)
+			hl7data.RquestOption.StartPageNum = strings.TrimSpace(config.RquestOption.StartPageNum)
+			hl7data.RquestOption.EndPageNum = strings.TrimSpace(config.RquestOption.EndPageNum)
+			hl7data.RquestOption.LastTouchStartDate = strings.TrimSpace(config.RquestOption.LastTouchStartDate)
+			hl7data.RquestOption.LastTouchEndDate = strings.TrimSpace(config.RquestOption.LastTouchEndDate)
 		}
 	} else {
 		fmt.Println(err)
